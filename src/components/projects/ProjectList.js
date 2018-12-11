744b32092708f2e9c584dad75777fe6e2ebe5d35
Gulnur Burganieva <gulnur.r23@gmail.com>
2018-12-12 03:31:32 +0400

    Dashboard has been add

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-lit section">
+      <div className="card z-depth-0 grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the ...</p>
+        <p className="grey-text">december</p>
+      </div>
+
+      <div className="card z-depth-0 grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the ...</p>
+        <p className="grey-text">december</p>
+      </div>
+
+      <div className="card z-depth-0 grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the ...</p>
+        <p className="grey-text">december</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
