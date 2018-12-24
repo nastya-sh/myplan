25a065dfa52651c3e3fe429f45be158f36b1e191
Gulnur Burganieva <gulnur.r23@gmail.com>
2018-12-25 02:59:26 +0400

    Redux/Dummy Data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="project-lit section">
       <div className="card z-depth-0 grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted by the ...</p>
         <p className="grey-text">december</p>
       </div>
