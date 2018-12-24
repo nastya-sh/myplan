2c9b90a9aae7f656c33d1cfc3e45a7b80758e1b4
Gulnur Burganieva <gulnur.r23@gmail.com>
2018-12-25 01:51:10 +0400

    Added nav link and css

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  projects: [
+    { id: "1", title: "Example Example Example Example Example" },
+    { id: "2", title: "3535 3535353" },
+    { id: "3", title: "vnvcncncv   hdfhd h hdhdfh hf" }
+  ]
+};
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
