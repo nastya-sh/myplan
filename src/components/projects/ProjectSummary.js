1c83118199a49e46bc751be718e02751d5922e91
Gulnur Burganieva <gulnur.r23@gmail.com>
2019-01-08 23:57:14 +0400

    lessons 30/31

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,17 @@
 import React from "react";
+import moment from "moment";
 
 const ProjectSummary = ({ project }) => {
   return (
     <div className="project-lit section">
       <div className="card z-depth-0 grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by the ...</p>
-        <p className="grey-text">december</p>
+        <p>
+          Posted by {project.authorFirstName} {project.authorLastName}
+        </p>
+        <p className="grey-text">
+          {moment(project.createdAt.toDate()).calendar()}
+        </p>
       </div>
     </div>
   );
