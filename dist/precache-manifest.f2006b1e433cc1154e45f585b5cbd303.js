58dd83f1786916705ee27f259b45bbc8a4dec133
Burganieva Gulnur <gulnur.r23@gmail.com>
2019-04-16 02:43:06 +0400

    change

---
+++ dist/precache-manifest.f2006b1e433cc1154e45f585b5cbd303.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb532e7332b5b44970a7",
+    "url": "/static/js/main.eb532e73.chunk.js"
+  },
+  {
+    "revision": "251e665bb52f1c4b1eea",
+    "url": "/static/js/1.251e665b.chunk.js"
+  },
+  {
+    "revision": "eb532e7332b5b44970a7",
+    "url": "/static/css/main.7bda659c.chunk.css"
+  },
+  {
+    "revision": "36a92aaf2f94dde6c1d5558712cdba2f",
+    "url": "/index.html"
+  }
+];
